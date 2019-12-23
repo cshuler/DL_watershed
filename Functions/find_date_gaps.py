8a90282e82faa2ad0183efd17987a2581a8d4b49
cshuler <chris@shulerhydrologic.com>
2019-12-23 06:07:59 -0500

    Create find_date_gaps.py

---
+++ Functions/find_date_gaps.py
@@ -0,0 +1,46 @@
+### This is used to Identify and label places where there are gaps in the data
+
+"""dataset_raw is any time series pandas dataframe that has datetime objects as its index
+   Colinterest is the data column that you want to look for gaps in """
+
+def find_date_gaps(dataset_raw, Colinterest):
+
+    a = dataset_raw[Colinterest].values  # Extract out relevant column from dataframe as array
+    ## Now gap filling time: 
+    # First: all of this is to go through each dataset and identify the start and the end of each gap. 
+
+    m = np.concatenate(( [True], np.isnan(a), [True] ))  # Mask
+    ss = np.flatnonzero(m[1:] != m[:-1]).reshape(-1,2)   # Start-stop limits
+
+    gapstarts_L = []; gapends_L = []
+    # this is if the series started late, i.e. the first gap is at the beginning
+    if ss[0][0] != 0:     
+        gapstart = 0
+        gapstarts_L.append(gapstart)
+        for m in ss:
+            gapstart2 = m[1]
+            gapstarts_L.append(gapstart2)
+            gapends_L.append(m[0])
+        del gapstarts_L[-1]                       # the end number is not the start of a new gap so delete this 
+
+    # this is if the series starst at the beginning of the data period, first gap is in the middle 
+    for m in ss:
+        gapstarts_L.append(m[1])
+        gapends_L.append(m[0])
+    del gapstarts_L[-1]
+    del gapends_L[0]
+
+    gapstarts_Dates = []; gapends_Dates = []
+    for h in gapstarts_L:
+        date5 = dataset_raw.index[h]
+        gapstarts_Dates.append(date5)   # this is a list of the starting date of all gaps
+    for h in gapends_L:
+        date5 = dataset_raw.index[h]
+        gapends_Dates.append(date5)     # this is a list of all the ending dates of the gaps
+           
+    # list the 
+    for idx, val in enumerate(gapstarts_Dates):
+        length = gapends_Dates[idx] -gapstarts_Dates[idx]
+        print("gap {} is {} from {} to {}".format(idx, length, gapstarts_Dates[idx], gapends_Dates[idx] ))
+    
+    return gapstarts_Dates, gapends_Dates
