c70a37fedcc7f4ba1dcddbb5b7e2cfba9adb1749
cshuler <chris@shulerhydrologic.com>
2019-12-23 06:40:28 -0500

    Update find_date_gaps.py

--- Functions/find_date_gaps.py
+++ Functions/find_date_gaps.py
@@ -3,6 +3,11 @@
 """dataset_raw is any time series pandas dataframe that has datetime objects as its index
    Colinterest is the data column that you want to look for gaps in """
 
+import os
+import pandas as pd
+import numpy as np
+   
+   
 def find_date_gaps(dataset_raw, Colinterest):
 
     a = dataset_raw[Colinterest].values  # Extract out relevant column from dataframe as array
