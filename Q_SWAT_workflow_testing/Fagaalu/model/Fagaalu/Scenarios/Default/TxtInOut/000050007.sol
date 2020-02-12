e3d182af77186343ed46e234dc9ce41cedae3826
cshuler <chris@shulerhydrologic.com>
2020-02-11 22:43:35 -1000

    made hydrograph!

---
+++ Q_SWAT_workflow_testing/Fagaalu/model/Fagaalu/Scenarios/Default/TxtInOut/000050007.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:37 Subbasin:5 HRU:7 Luse:URHD Soil: URBAN LAND Slope: 1.0-3.0 2/11/2020 20:48:02 QSWAT Workflow v1.5.2
+ Soil Name: URBAN LAND
+ Soil Hydrologic Group: D
+ Maximum rooting depth(mm) :  150.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : VAR
+ Depth                [mm]:      150.00
+ Bulk Density Moist [g/cc]:        0.00
+ Ave. AW Incl. Rock Frag  :        0.00
+ Ksat. (est.)      [mm/hr]:        0.01
+ Organic Carbon [weight %]:        0.00
+ Clay           [weight %]:        0.00
+ Silt           [weight %]:        0.00
+ Sand           [weight %]:        0.00
+ Rock Fragments   [vol. %]:        0.00
+ Soil Albedo (Moist)      :        0.16
+ Erosion K                :        0.00
+ Salinity (EC, Form 5)    :        0.00
+ Soil pH                  :        0.00
+ Soil CACO3               :        0.00
+                              
